ce28b262899de840b751091a09973ef1b52485ee
Corey Thompson <spikerulez@gmail.com>
2013-05-10 17:32:02 +1000

    Finished PassengerCar class

--- asgn2RollingStock/PassengerCar.java
+++ asgn2RollingStock/PassengerCar.java
@@ -11,12 +11,49 @@ import asgn2Exceptions.TrainException;
  */
 public class PassengerCar extends RollingStock{
 
+	private Integer numOfPassengers = 0;
+	private Integer numOfSeats = 0;
+	
 	/**
 	 * @throws TrainException 
 	 * 
 	 */
 	public PassengerCar(Integer grossWeight, Integer numberOfSeats) throws TrainException {
 		super(grossWeight);
+	}
+	
+	public void alight(Integer departingPassengers) throws TrainException {
+		if(departingPassengers < 0 || departingPassengers > numOfPassengers) {
+			throw new TrainException("Invalid number of departing passengers.");
+		} else {
+			numOfPassengers -= departingPassengers;
+		}
+	}
+	
+	public Integer board(Integer newPassengers) throws TrainException {
+		if(newPassengers >= 0) {
+			int numOfSeatsAvailable = numOfSeats - numOfPassengers;
+			int remainingPassengers = newPassengers - numOfSeatsAvailable;
+			if(remainingPassengers < 0) {
+			remainingPassengers = 0;
+			}
+			if(newPassengers > numOfSeatsAvailable) {
+			numOfPassengers += numOfSeatsAvailable;
+			} else {
+			numOfPassengers += newPassengers;
+			}
+			return remainingPassengers;
+		} else {
+			throw new TrainException("Invalid number of passengers.");
+		}
+	}
+	
+	public Integer numberOnBoard() {
+		return numOfPassengers;
+	}
+	
+	public Integer numberOfSeats() {
+		return numOfSeats;
 	}
 
 }
